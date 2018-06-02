fa82936170791473b0feca1a599964467b2fe483
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-02 16:16:25 -0400

    Updated from Brython Server: 6/2/2018 4:16:25 PM

--- chap6Tech.py
+++ chap6Tech.py
@@ -17,14 +17,16 @@ print('number of boxes = ',n)
 #LRAM
 totalLRAM = 0
 for i in range(0,n):
-    area = (a*(i*(x2-x1)/n)**b + c*(i*(x2-x1)/n)**d + e)*(x2-x1)/n
+    area = (a*((i*(x2-x1)/n)+x1)**b + c*((i*(x2-x1)/n)+x1)**d + e)*(x2-x1)/n
+    print(area)
     totalLRAM += area
 print('LRAM =',totalLRAM)
 
 #RRAM
 totalRRAM = 0
 for i in range(1,n+1):
-    area = (a*(i*(x2-x1)/n)**b + c*(i*(x2-x1)/n)**d + e)*(x2-x1)/n
+    area = (a*((i*(x2-x1)/n)+x1)**b + c*((i*(x2-x1)/n)+x1)**d + e)*(x2-x1)/n
+    print(area)
     totalRRAM += area
 print('RRAM =',totalRRAM)
 
@@ -33,7 +35,7 @@ trap = (totalLRAM+totalRRAM)/2
 print('Trap =',trap)
 
 #Actual using fundamental theorem of calculus
-print('Actual =',(a/(b+1))*(x2)**(b+1)+(c/(d+1))*(x2)**(d+1)+e*(x2)-(a/(b+1))*(x1)**(b+1)+(c/(d+1))*(x1)**(d+1)+e*(x1))
+print('Actual =',(a/(b+1))*(x2)**(b+1)+(c/(d+1))*(x2)**(d+1)+e*(x2)-((a/(b+1))*(x1)**(b+1)+(c/(d+1))*(x1)**(d+1)+e*(x1)))
 
 
 
