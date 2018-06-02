7bde0428c620a933195bc9ad8f2417fa1ae0f4cb
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-02 15:58:27 -0400

    Updated from Brython Server: 6/2/2018 3:58:27 PM

--- chap6Tech.py
+++ chap6Tech.py
@@ -1,14 +1,43 @@
 #Liam Collins
 #inputting polynomial function and outputting LRAM, RRAM and Trap
 
-print('ax^b + cx^d + ex^f + g')
+print('f(x) = ax^b + cx^d + e   [x1,x2]')
 a = float(input('Enter value for a: '))
 b = float(input('Enter value for b: '))
 c = float(input('Enter value for c: '))
 d = float(input('Enter value for d: '))
 e = float(input('Enter value for e: '))
-f = float(input('Enter value for f: '))
-g = float(input('Enter value for g: '))
+x1 = float(input('Enter x1 in [x1,x2]: '))
+x2 = float(input('Enter x2 in [x1,x2]: '))
+n = int(input('Enter the number of boxes: '))
+print('f(x) = ',a,'x^',b,' + ',c,'x^',d,' + ',e)
+print('Interval = [',x1,',',x2,']')
+print('number of boxes = ',n)
+
+#LRAM
+totalLRAM = 0
+for i in range(0,n):
+    area = (a*(i*(x2-x1)/n)**b + c*(i*(x2-x1)/n)**d + e)*(x2-x1)/n
+    print(area)
+    totalLRAM += area
+print('LRAM =',totalLRAM)
+
+#RRAM
+totalRRAM = 0
+for i in range(1,n+1):
+    area = (a*(i*(x2-x1)/n)**b + c*(i*(x2-x1)/n)**d + e)*(x2-x1)/n
+    print(area)
+    totalRRAM += area
+print('RRAM =',totalRRAM)
+
+#Trap = LRAM+RRAM/2
+trap = (totalLRAM+totalRRAM)/2
+
+
+    
+    
+    
+
 
 
 
