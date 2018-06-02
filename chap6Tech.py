b47acf9ade0043f78b65b03b34023c6e86aecc9d
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-02 15:28:58 -0400

    Updated from Brython Server: 6/2/2018 3:28:57 PM

--- chap6Tech.py
+++ chap6Tech.py
@@ -1,1 +1,14 @@
+#Liam Collins
+#inputting polynomial function and outputting LRAM, RRAM and Trap
+
+print('ax^b + cx^d + ex^f + g')
+a = float(input('Enter value for a: '))
+b = float(input('Enter value for b: '))
+c = float(input('Enter value for c: '))
+d = float(input('Enter value for d: '))
+e = float(input('Enter value for e: '))
+f = float(input('Enter value for f: '))
+g = float(input('Enter value for g: '))
+
+
 
