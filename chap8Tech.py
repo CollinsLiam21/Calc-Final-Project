87445cacbccda6d8c2009600e32c8612eb81e646
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-03 21:48:08 -0400

    Updated from Brython Server: 6/3/2018 9:48:06 PM

--- chap8Tech.py
+++ chap8Tech.py
@@ -2,6 +2,8 @@
 #chap8Tech.py
 #inputting polynomial function and interval, and outputting arc length using simpson's rule
 
+from math import sqrt
+
 def arcLength(a,b,c,d,e,x1,x2):
     print('f(x) = ',a,'x^',b,' + ',c,'x^',d,' + ',e)
     print('Interval = [',x1,',',x2,']')
@@ -9,18 +11,18 @@ def arcLength(a,b,c,d,e,x1,x2):
     #Finding derivative
     print("f'(x) = ",a*b,'x^',b-1,' + ',c*d,'x^',d-1)
     
-    widthOfBox = 0.00002
+    n = 10
     
     num = 1
     total = 0
-    for i in range(x1,x2,2/5):
+    for i in range(1,n):
         if num%2 == 0:
-            total += 4*(a*b*(i)**(b-1)+c*d*(i)**(d-1))
+            total += 4*sqrt(1+a*b*(((x2-x1)/n)*i+x1)**(b-1)+c*d*(((x2-x1)/n)*i+x1)**(d-1))
             num += 1
         else:
-            total += 2*(a*b*(i)**(b-1)+c*d*(i)**(d-1))
+            total += 2*sqrt(1+a*b*(((x2-x1)/n)*i+x1)**(b-1)+c*d*(((x2-x1)/n)*i+x1)**(d-1))
             num += 1
     
-    print(total)
+    print((total+sqrt(1+a*b*(x1)**(b-1)+c*d*(x1)**(d-1))+sqrt(1+a*b*(x2)**(b-1)+c*d*(x2)**(d-1))*(x2-x1)/n*(1/3))
 
-arcLength(1,1,2,2,3,2,2)
+arcLength(1,1,2,2,3,2,3)
