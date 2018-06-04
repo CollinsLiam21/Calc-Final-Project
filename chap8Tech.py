9d7f5b291435bde60669a41c2b4bb2b1bdcef62b
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-03 21:53:58 -0400

    Updated from Brython Server: 6/3/2018 9:53:56 PM

--- chap8Tech.py
+++ chap8Tech.py
@@ -26,6 +26,6 @@ def arcLength(a,b,c,d,e,x1,x2):
     firstYValue = sqrt(1+(a*b*(x1)**(b-1)+c*d*(x1)**(d-1))**2)
     secondYValue = sqrt(1+(a*b*(x2)**(b-1)+c*d*(x2)**(d-1))**2)
     
-    print((total+firstYValue+secondYValue)*(x2-x1)/n*(1/3)))
+    print((total+firstYValue+secondYValue)*(x2-x1)/n*(1/3))
 
 arcLength(1,1,2,2,3,2,3)
