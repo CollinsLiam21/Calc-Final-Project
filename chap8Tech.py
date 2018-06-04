caf7ba1d951aaa01c3c50af868df672e08351c8e
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-03 22:16:38 -0400

    Updated from Brython Server: 6/3/2018 10:16:36 PM

--- chap8Tech.py
+++ chap8Tech.py
@@ -1,6 +1,6 @@
 #Liam Collins
 #chap8Tech.py
-#inputs polynomial function and interval, and outputs arc length using simpson's rule
+#inputs polynomial function and interval, outputs arc length approximated using simpson's rule
 
 from math import sqrt
 
@@ -26,7 +26,7 @@ def arcLength(a,b,c,d,e,x1,x2):
     firstYValue = sqrt(1+(a*b*(x1)**(b-1)+c*d*(x1)**(d-1))**2)
     secondYValue = sqrt(1+(a*b*(x2)**(b-1)+c*d*(x2)**(d-1))**2)
     
-    print(round((total+firstYValue+secondYValue)*(x2-x1)/n*(1/3),2))
+    print((total+firstYValue+secondYValue)*(x2-x1)/n*(1/3))
 
 arcLength(1/3,3,1/4,-1,0,1,3)
 
