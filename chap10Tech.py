07a2057f92b393d1fc7142d72a851c0b9e048b5d
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-04 21:32:17 -0400

    Updated from Brython Server: 6/4/2018 9:32:17 PM

--- chap10Tech.py
+++ chap10Tech.py
@@ -1,1 +1,9 @@
+#Liam Collins
+#chap10Tech.py
+#input power series and center, output first four terms of taylor series, and integral of taylor series
 
+def powerSeries(x,center):
+    
+    print('f(x) = 1/(1-'+x+') centered at x =',center)
+
+powerSeries('x^2',2)
