a293a2619277cde0933378c1b091db26fe695173
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-04 21:41:43 -0400

    Updated from Brython Server: 6/4/2018 9:41:42 PM

--- chap10Tech.py
+++ chap10Tech.py
@@ -2,8 +2,10 @@
 #chap10Tech.py
 #input power series and center, output first four terms of taylor series, and integral of taylor series
 
-def powerSeries(x,center):
+def powerSeries(a,x,center):
     
-    print('f(x) = 1/(1-'+x+') centered at x =',center)
+    print('f(x) = '+str(a)+'/(1-'+x+') centered at x =',center)
+    
+    print(str(a)+'/(1-'+x+') = '+str(a)+'('+x+'-'+str(a)+')'+'^0 + '+str(a)+'('+x+'-'+str(a)+')'+'^1 + ')
 
-powerSeries('x^2',2)
+powerSeries(3,'x',2)
