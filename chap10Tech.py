19e27ecbd7b991cfad14bba412d181569654b076
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-04 21:44:06 -0400

    Updated from Brython Server: 6/4/2018 9:44:05 PM

--- chap10Tech.py
+++ chap10Tech.py
@@ -6,6 +6,6 @@ def powerSeries(a,x,center):
     
     print('f(x) = '+str(a)+'/(1-'+x+') centered at x =',center)
     
-    print(str(a)+'/(1-'+x+') = '+str(a)+'('+x+'-'+str(a)+')'+'^0 + '+str(a)+'('+x+'-'+str(a)+')'+'^1 + ')
+    print(str(a)+'/(1-'+x+') = '+str(a)+'('+x+'-'+str(a)+')'+'^0 + '+str(a)+'('+x+'-'+str(a)+')'+'^1 + '+str(a)+'('+x+'-'+str(a)+')'+'^2 + '+str(a)+'('+x+'-'+str(a)+')'+'^3')
 
-powerSeries(3,'x',2)
+powerSeries(3,'x^2',2)
