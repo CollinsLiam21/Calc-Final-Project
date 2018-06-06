e7c0ca0b8656ad7f0c20b9b6cfc3c117c57b2c32
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-06-06 10:09:41 -0400

    Updated from Brython Server: 6/6/2018 10:09:40 AM

--- chap8Tech.py
+++ chap8Tech.py
@@ -4,15 +4,19 @@
 
 from math import sqrt
 
+#defined function
 def arcLength(a,b,c,d,e,x1,x2):
+    #prints function and interval
     print('f(x) = ',a,'x^',b,' + ',c,'x^',d,' + ',e)
     print('Interval = [',x1,',',x2,']')
     
     #Finding derivative
     print("f'(x) = ",a*b,'x^',b-1,' + ',c*d,'x^',d-1)
     
-    n = 1000
+    #number of boxes, after 1000 boxes the program runs very slowly
+    n = 100
     
+    #a loop multiplying every other y value by 4 and the other y values by 2 (Simpson's Rule)
     num = 1
     total = 0
     for i in range(1,n):
@@ -23,11 +27,13 @@ def arcLength(a,b,c,d,e,x1,x2):
             total += 2*sqrt(1+(a*b*(((x2-x1)/n)*i+x1)**(b-1)+c*d*(((x2-x1)/n)*i+x1)**(d-1))**2)
             num += 1
     
+    #first value yo and last value yn
     firstYValue = sqrt(1+(a*b*(x1)**(b-1)+c*d*(x1)**(d-1))**2)
-    secondYValue = sqrt(1+(a*b*(x2)**(b-1)+c*d*(x2)**(d-1))**2)
+    lastYValue = sqrt(1+(a*b*(x2)**(b-1)+c*d*(x2)**(d-1))**2)
     
-    print('Arc Length:',(total+firstYValue+secondYValue)*(x2-x1)/n*(1/3))
+    #prints arc length
+    print('Arc Length:',(total+firstYValue+lastYValue)*(x2-x1)/n*(1/3))
 
+#user input values for the function
+#function(a,b,c,d,e,x1,x2)
 arcLength(1/3,3,1/4,-1,0,1,3)
-
-
